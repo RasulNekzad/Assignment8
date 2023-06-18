e7276c86324b20ed312554de1b6ee4a3cd79cdec
rasulnekzad <rasulnekzad@Rasuls-MacBook-Air.local>
2023-06-18 17:14:28 -0400

    added styling

--- src/App.js
+++ src/App.js
@@ -18,26 +18,33 @@ function App() {
     const result = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${apiKey}`);
     setGifs(result.data.data);
     setIsTrending(false);
+    setIsLoading(false);
   }
 
   function fetchTrendingGifs() {
     setIsLoading(true);
     const result = axios.get(`http://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`)
-    .then((result) => setGifs(result.data.data))
+    .then((result) => {
+      setGifs(result.data.data)
+      setIsLoading(false);
+    })
   }
 
   useEffect(() => fetchTrendingGifs(), []);
   useEffect(()=>{
     console.log(gifs)
-    setIsLoading(false);
   }, [gifs])
 
   return (
     <div className="App">
       <SearchField setQuery ={setQuery} fetchGifs={fetchGifs}/>
-      {isTrending ? <p>Trending Gifs</p> : <p>Your Search Results: </p>}
+      {isTrending ? <h1 id="trending">Trending Gifs</h1> : <h1 id="search">Your Search Results: </h1>}
       {!isLoading ? gifs.map((gif, index) => {
-        return (<GifCard gif={gif.images.original.url} key={index} />)
+        return (
+        <div className='container'>
+          <GifCard gif={gif.images.original.url} key={index} />
+        </div>
+         )
       }) : (<img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921%22%20decoding=%22async" />) }
     </div>
   );
